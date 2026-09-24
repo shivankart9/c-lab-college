ae668a69c4c517a3c393cd0a151723a4c49715c1
Shivankar Tiwari <shivankart9@gmail.com>
2026-09-24 10:33:28 +0530

    created program to check whether number is odd or even

---
+++ even-odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() { 
+int num;
+printf("enter the number : ");
+scanf("%d",&num);
+
+if(num%2 == 0) {
+printf("input number is even ");
+} else {
+printf("input number is odd ");
+}
+
+return 0;
+}
