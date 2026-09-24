a44462a5e64580371486aac51481f651ee41bc90
Shivankar Tiwari <shivankart9@gmail.com>
2026-09-24 10:22:49 +0530

    Implement area calculation for triangle
    Calculates the area of a triangle based on user input for length and width.

---
+++ area-of-triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+  int len,wid;
+  printf("enter the length of triangle : ");
+  scanf("%d",&len);
+  printf("enter the width of the triangle : ");
+  scanf("%d",&wid);
+
+  printf("\nthe area of the triangle is : %d",len*wid);
+
+return 0;
+}
